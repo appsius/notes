7e890cfeae26bb19d03516a5ae4feb0868bbe4c9
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-24 14:48:47 +0300

    Refactoring Notes-App

--- notes-app.js
+++ notes-app.js
@@ -1,48 +1,19 @@
-let notes = [];
+const notes = getSavedNotes();
 
 const filters = {
 	searchText: '',
 };
 
-// Check if there is any saved data
-const notesJSON = localStorage.getItem('notes');
-
-if (notesJSON !== null) {
-	notes = JSON.parse(notesJSON);
-}
-
-const renderNotes = function (notes, filters) {
-	const filteredNotes = notes.filter(function (note) {
-		return note.title
-			.toLowerCase()
-			.includes(filters.searchText.toLowerCase());
-	});
-
-	document.querySelector('#notes').innerHTML = '';
-
-	filteredNotes.forEach(function (note) {
-		const noteEl = document.createElement('p');
-
-		if (note.title.length > 0) {
-			noteEl.textContent = note.title;
-		} else {
-			noteEl.textContent = 'Unnamed biggy..';
-		}
-
-		document.querySelector('#notes').appendChild(noteEl);
-	});
-};
-
 renderNotes(notes, filters);
 
+// Event Handlers
 document.querySelector('#add-notes').addEventListener('click', function (e) {
 	notes.push({
 		title: '',
 		body: '',
 	});
 
-	localStorage.setItem('notes', JSON.stringify(notes));
-
+	saveNotes(notes);
 	renderNotes(notes, filters);
 });
 
