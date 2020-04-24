de039e2e27d5a370f3520c16818a8a66df945b19
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-25 02:02:06 +0300

    Syncing Data Accross Pages

--- notes-edit.js
+++ notes-edit.js
@@ -3,8 +3,8 @@ const bodyElement = document.querySelector('#note-body');
 const removeElement = document.querySelector('#remove-note');
 
 const noteId = location.hash.substring(1);
-const notes = getSavedNotes();
-const note = notes.find(function (note) {
+let notes = getSavedNotes();
+let note = notes.find(function (note) {
 	return note.id === noteId;
 });
 
@@ -29,4 +29,21 @@ removeElement.addEventListener('click', function (e) {
 	removeNote(note.id);
 	saveNotes(notes);
 	location.assign('/index.html');
+});
+
+window.addEventListener('storage', function (e) {
+	if (e.key === 'notes') {
+		notes = JSON.parse(e.newValue);
+
+		note = notes.find(function (note) {
+			return note.id === noteId;
+		});
+
+		if (note === undefined) {
+			location.assign('/index.html');
+		}
+
+		titleElement.value = note.title;
+		bodyElement.value = note.body;
+	}
 });
