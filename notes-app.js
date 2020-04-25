b64da457f58134a0a96d25b3ede32e8c19d613eb
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-25 18:11:57 +0300

    Turns Regular Functions into Arrow Functions

--- notes-app.js
+++ notes-app.js
@@ -8,7 +8,7 @@ const filters = {
 renderNotes(notes, filters);
 
 // Event Handlers
-document.querySelector('#add-notes').addEventListener('click', function (e) {
+document.querySelector('#add-notes').addEventListener('click', (e) => {
 	const id = uuidv4();
 	const timestamp = moment().valueOf();
 
@@ -24,17 +24,17 @@ document.querySelector('#add-notes').addEventListener('click', function (e) {
 	location.assign(`/edit.html#${id}`);
 });
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', (e) => {
 	filters.searchText = e.target.value;
 	renderNotes(notes, filters);
 });
 
-document.querySelector('#filter-by').addEventListener('change', function (e) {
+document.querySelector('#filter-by').addEventListener('change', (e) => {
 	filters.sortBy = e.target.value;
 	renderNotes(notes, filters);
 });
 
-window.addEventListener('storage', function (e) {
+window.addEventListener('storage', (e) => {
 	if (e.key === 'notes') {
 		notes = JSON.parse(e.newValue);
 	}
