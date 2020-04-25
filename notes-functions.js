b3593c442ddc6096b2df42bf7bef5d95bfd91592
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-26 00:12:39 +0300

    Try and Catch Error Handling

--- notes-functions.js
+++ notes-functions.js
@@ -1,7 +1,12 @@
 // Get existing localStorage data
 const getSavedNotes = () => {
 	const notesJSON = localStorage.getItem('notes');
-	return notesJSON ? JSON.parse(notesJSON) : [];
+
+	try {
+		return notesJSON ? JSON.parse(notesJSON) : [];
+	} catch (e) {
+		return [];
+	}
 };
 
 // Save notes to the localStorage
